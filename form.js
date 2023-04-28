2c75b65e0793c3c5d6277d8904d5b8d597f81cd0
sansiil <sahanshiludash@gmail.com>
2023-04-29 00:48:05 +0545

    change page fn

--- form.js
+++ form.js
@@ -1,13 +1,81 @@
 
-function nexxt(){
-    const fname=document.getElementById('fname').value;
-    const lname=document.getElementById('lname').value;
-    if(fname!='' && lname!=''){
-        const pg1=document.getElementById("pg1");
-        const pg2=document.getElementById("pg2");
-        pg1.style.display="none";
-        pg2.style.display="block";
+//go to next or back page
+
+const changePage=(num,direction)=>{
+    //for num number to change color to grey
+    
+    const loosescolor=document.getElementById('number'+num);
+    loosescolor.style.background='rgba(128, 128, 128,0.1)';
+    loosescolor.style.color='grey';
+
+    ///display to none for num
+    const pagetohide=document.getElementById('pg'+num);
+    pagetohide.style.display='none';
+
+    //next page if direction is positive
+    //else back page
+
+    if (direction>0){
+        num++;
     }
-    else
-      alert(1);
-}
+    else{
+        num--;
+    }
+    
+    
+    //for num++ number to change color to green
+    const colorbutton=document.getElementById('number'+num);
+    colorbutton.style.background='rgb(77, 184, 77)';
+    colorbutton.style.color='white';
+
+    //display to flex for num++
+    const pagetoshow=document.getElementById('pg'+num);
+    pagetoshow.style.display='flex';
+
+};
+
+
+
+// const form=()=>{
+//     const forms=document.querySelectorAll('form');
+//     forms.forEach(form=>{
+//         form.onsubmit(events=>{
+//             events.preventDefault();
+
+//             events.target.reset();
+//         })
+//     })
+// }
+
+// document.querySelector('.filebox').addEventListener('dragover',()=>0)
+
+// document.querySelectorAll('#next').forEach(button=>{
+//     button.disabled=true;
+// })
+
+
+ 
+// formfilled();
+
+
+// document.getElementById('next').addEventListener('mouseover',()=>{
+     
+//     const formfilled=()=>{
+//         const input=document.querySelectorAll('#pg1 input');
+//         document.querySelectorAll('#pg1 input').forEach(isfilled=>{
+//             if(isfilled.value!=='' && isfilled.required){
+//                  document.querySelector('#next').style.cursor='pointer';
+//                  document.querySelector('#next').disabled=false;
+    
+//             }
+//             else{
+//                 console.log('aaa');
+                
+//             }
+//         })
+    
+//      };
+
+//      formfilled();
+    
+// })
